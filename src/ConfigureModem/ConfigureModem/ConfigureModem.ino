cf0e61e3cbdd911ad49381b3ef37d94f2ab7dc74
Mitch Denny <mitch@mitchdenny.com>
2015-10-30 11:36:10 +1100

    Empty Visual Micro project. Will be putting a sketch in here to configure the BlueSMiRF modem.

---
+++ src/ConfigureModem/ConfigureModem/ConfigureModem.ino
@@ -0,0 +1,14 @@
+// The purpose of this sketch is to quickly configure the Sparkfun BlueSMiRF
+// Silver bluetooth modem to a known baud rate so that it can used with the
+// Arduino Uno and remote wiring API from a UWP application.
+
+#include <SoftwareSerial.h>
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
