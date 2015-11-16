4762f21249e163c456e069494991daada0d96f90
Mitch Denny <mitch.denny@readify.net>
2015-11-16 23:39:49 +1000

    Progress on Sysex. But not sure its a good implementation yet.

--- src/Demo/ReadUsingCustomProtocol/MainPage.xaml.cs
+++ src/Demo/ReadUsingCustomProtocol/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Maker.Serial;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
--- src/Demo/ReadUsingFirmataProtocol/MainPage.xaml.cs
+++ src/Demo/ReadUsingFirmataProtocol/MainPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.Maker.Firmata;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +30,45 @@ namespace ReadUsingFirmataProtocol
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private BluetoothSerial bluetooth = null;
+        private RemoteDevice device = null;
+        private UwpFirmata firmata = null;
+
+        public async Task CaptureReadings()
+        {
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.bluetooth = new BluetoothSerial("RNBT-9E86");
+                this.firmata = new UwpFirmata();
+                this.firmata.FirmataConnectionReady += Firmata_FirmataConnectionReady;
+                this.firmata.StringMessageReceived += Firmata_StringMessageReceived;
+
+                this.device = new RemoteDevice(firmata);
+                this.firmata.begin(bluetooth);
+                bluetooth.begin(9600, SerialConfig.SERIAL_8N1);
+            });
+        }
+
+        private async void Firmata_StringMessageReceived(UwpFirmata caller, StringCallbackEventArgs argv)
+        {
+            var content = argv.getString();
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.ReadingTextBlock.Text = content;
+            });
+        }
+
+        private void Firmata_FirmataConnectionReady()
+        {
+            byte ULTRASONIC_DISTANCE_QUERY = 0x42;
+            this.firmata.sendSysex(ULTRASONIC_DISTANCE_QUERY, new byte[] { 0 }.AsBuffer());
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(this.CaptureReadings);
         }
     }
 }
